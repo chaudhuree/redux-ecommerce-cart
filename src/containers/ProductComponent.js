b5ff3eedeae1eed64aa2062f892f86e076cbd89e
chaudhuree <chaudhuree00@gmail.com>
2022-04-02 20:28:51 +0600

    docs: set fetch data nad style product component

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function ProductComponent() {
-
   return (
-    <div>ProductComponent</div>
-  )
+    <div className="four wide column">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header"></div>
+            <div className="meta price">$ </div>
+            <div className="meta"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductComponent;
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import ProductComponent from './ProductComponent';
 
-function ProductListing() {
+export default function ProductListing() {
   const products = useSelector(state => state.productReducer.products);
   console.log(products);
+
+  const fetchData= async()=>{
+     const response = await axios.get('https://fakestoreapi.com/products').catch(err=>{
+        console.log(err);
+      });
+      console.log(response.data);
+  }
+  useEffect(() => { 
+    fetchData();
+  }, []);
+
   return (
-    <div className='ui gird container'>ProductListing</div>
+    <div className='ui gird container'><ProductComponent/></div>
   )
 }
-
-export default ProductListing
